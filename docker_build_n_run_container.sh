d4ca19e4e86c235a65da3b3651ca4b0011030883
taaphise <taaphise@gmail.com>
2024-12-29 21:47:31 -0500

    updated

--- docker_build_n_run_container.sh
+++ docker_build_n_run_container.sh
@@ -7,7 +7,7 @@ bash docker_install.sh
 bash docker_pull_image.sh
 
 # Read the container name from docker_cfgs.txt
-CONTAINER_NAME=$(grep 'container:' docker_cfgs.txt | cut -d ':' -f2 | xargs)
+CONTAINER_NAME=$(grep 'container_name:' docker_cfgs.txt | cut -d ':' -f2 | xargs)
 
 # Run the Docker container
 echo "Running the Docker container: $CONTAINER_NAME"
